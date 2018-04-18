f841f22cc68270ad5a944bed41105706c9e4c101
Nikos <triantafyllou.ni@gmail.com>
2018-04-18 14:54:59 +0300

    eidas attributes multilang

--- studentApp/utils/authUtils.js
+++ studentApp/utils/authUtils.js
@@ -92,23 +92,24 @@ exports.userDetailsFromToken = (req,res) =>{
         console.log(token.sub);
         let result = JSON.parse(token.sub);
         result.eid = stripchar.RSExceptUnsAlpNum(result.eid);
+        //** ISS webapp returns the values as firstName,lastName, while webapp 2.0 as currentFamilyName,currentGivenName
+        //**
         if(!result.firstName){
-          if(result.currentGivenName.indexOf(",") > 0){
-             result.natFirstName = result.currentFamilyName.split(",")[1];
-             result.firstName = result.currentFamilyName.split(",")[0];;
-          }else{
             result.firstName = result.currentGivenName;
-          }
+                  }
+        if(!result.familyName){
+          result.familyName = result.currentFamilyName;
 
         }
-        if(!result.familyName){
-          if(result.currentFamilyName.indexOf(",") > 0){
-              result.natFamilyName = result.currentFamilyName.split(",")[1];
-              result.familyName = result.currentFamilyName.split(",")[0];
-          }else{
-            result.familyName = result.currentFamilyName;
-          }
+        if(result.firstName.indexOf(",") > 0){
+           result.natFirstName = result.firstName.split(",")[1];
+           result.firstName = result.firstName.split(",")[0];;
         }
+        if(result.familyName.indexOf(",") > 0){
+            result.natFamilyName = result.familyName.split(",")[1];
+            result.familyName = result.familyName.split(",")[0];
+        }
+
         result.userName = result.firstName+"_"+result.familyName;
         resolve(result);
       }
