01c7fe35ec2c81f589e2263be641a1d6e24f393c
Nikos <triantafyllou.ni@gmail.com>
2018-04-24 19:49:33 +0300

    download raw

--- studentApp/reactApp/src/components/supplementBtns.jsx
+++ studentApp/reactApp/src/components/supplementBtns.jsx
@@ -11,6 +11,9 @@ export default class SupBtns extends React.Component {
     const download = <Link to={"/back/supplement/rest/pdf/"+this.props.id} className="btn-floating btn-medium waves-effect waves-light blue darken-3" target="_blank">
                         <i className="material-icons">file_download</i>
                     </Link>;
+    const downloadRaw = <Link to={"/back/supplement/raw/pdf/"+this.props.id} className="btn-floating btn-medium waves-effect waves-light raw darken-3" target="_blank">
+                        <i className="material-icons">code</i>
+                    </Link>;
     const edit =  <Link to={"/app/edit/"+this.props.id} className="btn-floating btn-medium waves-effect waves-light yellow darken-3" style={{ marginLeft: "1em"}}>
                         <i className="material-icons">edit</i>
                     </Link>;
@@ -19,6 +22,7 @@ export default class SupBtns extends React.Component {
         return (
            <span>
               {download}
+              {downloadRaw}
               {edit}
            </span>);
       }else{
