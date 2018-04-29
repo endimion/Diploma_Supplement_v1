a7df0b037854e62c929c63866e8447371ed1b820
Nikos <triantafyllou.ni@gmail.com>
2018-04-29 23:43:57 +0300

    UI

--- studentApp/reactApp/src/components/navBar.jsx
+++ studentApp/reactApp/src/components/navBar.jsx
@@ -8,20 +8,22 @@ import '../styles/navbar.css'
 export default class NavigationBar extends React.Component {
 
 
-      render(){
-        const style = {
-          'zIndex': '998!important'
-        };
+  render(){
+    const style = {
+      'zIndex': '998!important',
+      'background-color':'#06114e!important'
+    };
 
 
-        const account = <li><NavLink to="/">Account</NavLink></li>;
-        return   <Navbar style={style} href="/app/" brand='DS Service' right fixed={true}>
-                 <li><NavLink to="/app/home">Home</NavLink></li>
-	               <li><NavLink to="/app/manage">Manage Supplements</NavLink></li>
-                 <li><NavLink to="/app/request">Request new Supplement</NavLink></li>
-                 <SideNavigation  trig={account} user= {this.props.user}/>
-              </Navbar>
+    const account = <li><NavLink to="/">Account</NavLink></li>;
 
-    }
+    return   <Navbar style={style} href="/app/" brand='DS Service' right fixed={true} className="blueBar">
+             <li><NavLink to="/app/home">Home</NavLink></li>
+             <li><NavLink to="/app/manage">Manage Supplements</NavLink></li>
+             <li><NavLink to="/app/request">Request new Supplement</NavLink></li>
+             <SideNavigation  trig={account} user= {this.props.user}/>
+          </Navbar>
+
+  }
 
 }
