02e18041c1d6b9948ce1b947448e826314e7ab37
Nikos <triantafyllou.ni@gmail.com>
2018-04-30 08:49:35 +0300

    UI changes

--- studentApp/reactApp/src/components/supplementBtns.jsx
+++ studentApp/reactApp/src/components/supplementBtns.jsx
@@ -6,15 +6,22 @@ import {Link} from 'react-router-dom'
 export default class SupBtns extends React.Component {
 
 
+  componentDidMount(){
+    $(document).ready(function(){
+     $('.tooltipped').tooltip();
+    });
+  }
+
+
   render(){
 
-    const download = <Link to={"/back/supplement/rest/pdf/"+this.props.id} className="btn-floating btn-medium waves-effect waves-light blue darken-3" target="_blank">
+    const download = <Link to={"/back/supplement/rest/pdf/"+this.props.id} className="btn btn-floating tooltipped btn-medium waves-effect waves-light blue darken-3" data-position="top" data-tooltip="Download pdf" target="_blank">
                         <i className="material-icons">file_download</i>
                     </Link>;
-    const downloadRaw = <Link to={"/back/supplement/rest/raw/"+this.props.id} className="btn-floating btn-medium waves-effect waves-light raw darken-3" target="_blank">
+    const downloadRaw = <Link to={"/back/supplement/rest/raw/"+this.props.id} className="btn btn-floating tooltipped btn-medium waves-effect waves-light raw darken-3" data-position="top" data-tooltip="Download XML/JSON" target="_blank">
                         <i className="material-icons">code</i>
                     </Link>;
-    const edit =  <Link to={"/app/edit/"+this.props.id} className="btn-floating btn-medium waves-effect waves-light yellow darken-3" style={{ marginLeft: "1em"}}>
+    const edit =  <Link to={"/app/edit/"+this.props.id} className="btn btn-floating tooltipped btn-medium waves-effect waves-light yellow darken-3" data-position="top" data-tooltip="Share" style={{ marginLeft: "1em"}}>
                         <i className="material-icons">edit</i>
                     </Link>;
 
